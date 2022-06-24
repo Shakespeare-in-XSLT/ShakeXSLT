ac7139f69474338204af3de0b43ec8ab21533bf9
sterenz <87770613+sterenz@users.noreply.github.com>
2022-06-24 18:27:36 +0200

    script and sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -61,38 +61,62 @@ fabTrigger.addEventListener("click", function() {
 
 });
 
+/***********************************************/
+/****************** THEME **********************/
+/***********************************************/
+
+// Create the theme object
+
+const theme = {
+  STYLE_1: 'style1600',
+  STYLE_2: 'style1890',
+  STYLE_3: 'style1920',
+  STYLE_4: 'style1950',
+  STYLE_5: 'style1990',
+  STYLE_6: 'style2040'
+}
+
+
+
 /**
  * Apply 1600 Theme.
  */
 let style1600Trigger = document.querySelector('.trigger1600');
 style1600Trigger.addEventListener("click", function() {
   
-  const style1890 = getItemInLocalStorage('style1890')
-  if (style1890) {
-    removeItemInLocalStorage('style1890');
+  const style2 = getItemInSessionStorage(theme.STYLE_2)
+  if (style2) {
+    removeItemInSessionStorage(theme.STYLE_2);
   }
-  const style1920 = getItemInLocalStorage('style1920')
-  if (style1920) {
-    removeItemInLocalStorage('style1920');
+  const style3 = getItemInSessionStorage(theme.STYLE_3)
+  if (style3) {
+    removeItemInSessionStorage(theme.STYLE_3);
   }
-  const style1950 = getItemInLocalStorage('style1950')
-  if (style1950) {
-    removeItemInLocalStorage('style1950');
+  const style4 = getItemInSessionStorage(theme.STYLE_4)
+  if (style4) {
+    removeItemInSessionStorage(theme.STYLE_4);
   }
-  const style1990 = getItemInLocalStorage('style1990')
-  if (style1990) {
-    removeItemInLocalStorage('style1990');
+  const style5 = getItemInSessionStorage(theme.STYLE_5)
+  if (style5) {
+    removeItemInSessionStorage(theme.STYLE_5);
   }
-  const style2040 = getItemInLocalStorage('style2040')
-  if (style2040) {
-    removeItemInLocalStorage('style2040');
+  const style6 = getItemInSessionStorage(theme.STYLE_6)
+  if (style6) {
+    removeItemInSessionStorage(theme.STYLE_6);
   }
 
-  setItemInLocalStorage('style1600','style1600');
+  setItemInSessionStorage(theme.STYLE_1,theme.STYLE_1);
   
-  document.body.classList.toggle('style1600');
+  document.body.classList.toggle(theme.STYLE_1);
   document.querySelector('#hero-image').src="img/hero/shakespeare_1600.png";
-  document.body.classList.remove('style1890','style1920','style1950','style1990','style2040','fab-open');
+  document.body.classList.remove(
+    theme.STYLE_2,
+    theme.STYLE_3,
+    theme.STYLE_4,
+    theme.STYLE_5,
+    theme.STYLE_6,
+    'fab-open'
+  );
 
 });
 
@@ -102,32 +126,39 @@ style1600Trigger.addEventListener("click", function() {
 let style1890Trigger = document.querySelector('.trigger1890');
 style1890Trigger.addEventListener("click", function() {
 
-  const style1600 = getItemInLocalStorage('style1600')
-  if (style1600) {
-    removeItemInLocalStorage('style1600');
+  const style1 = getItemInSessionStorage(theme.STYLE_1)
+  if (style1) {
+    removeItemInSessionStorage(theme.STYLE_1);
   }
-  const style1920 = getItemInLocalStorage('style1920')
-  if (style1920) {
-    removeItemInLocalStorage('style1920');
+  const style3 = getItemInSessionStorage(theme.STYLE_3)
+  if (style3) {
+    removeItemInSessionStorage(theme.STYLE_3);
   }
-  const style1950 = getItemInLocalStorage('style1950')
-  if (style1950) {
-    removeItemInLocalStorage('style1950');
+  const style4 = getItemInSessionStorage(theme.STYLE_4)
+  if (style4) {
+    removeItemInSessionStorage(theme.STYLE_4);
   }
-  const style1990 = getItemInLocalStorage('style1990')
-  if (style1990) {
-    removeItemInLocalStorage('style1990');
+  const style5 = getItemInSessionStorage(theme.STYLE_5)
+  if (style5) {
+    removeItemInSessionStorage(theme.STYLE_5);
   }
-  const style2040 = getItemInLocalStorage('style2040')
-  if (style2040) {
-    removeItemInLocalStorage('style2040');
+  const style6 = getItemInSessionStorage(theme.STYLE_6)
+  if (style6) {
+    removeItemInSessionStorage(theme.STYLE_6);
   }
 
-  setItemInLocalStorage('style1890','style1890');
+  setItemInSessionStorage(theme.STYLE_2,theme.STYLE_2);
 
-  document.body.classList.toggle('style1890');
+  document.body.classList.toggle(theme.STYLE_2);
   document.querySelector('#hero-image').src="img/hero/shakespeare_1890.png";
-  document.body.classList.remove('style1600','style1920','style1950','style1990','style2040','fab-open');
+  document.body.classList.remove(
+    theme.STYLE_1,
+    theme.STYLE_3,
+    theme.STYLE_4,
+    theme.STYLE_5,
+    theme.STYLE_6,
+    'fab-open'
+  );
 
 });
 
@@ -137,32 +168,39 @@ style1890Trigger.addEventListener("click", function() {
 let style1920Trigger = document.querySelector('.trigger1920');
 style1920Trigger.addEventListener("click", function() {
 
-  const style1600 = getItemInLocalStorage('style1600')
-  if (style1600) {
-    removeItemInLocalStorage('style1600');
+  const style1 = getItemInSessionStorage(theme.STYLE_1)
+  if (style1) {
+    removeItemInSessionStorage(theme.STYLE_1);
   }
-  const style1890 = getItemInLocalStorage('style1890')
-  if (style1890) {
-    removeItemInLocalStorage('style1890');
+  const style3 = getItemInSessionStorage(theme.STYLE_2)
+  if (style3) {
+    removeItemInSessionStorage(theme.STYLE_2);
   }
-  const style1950 = getItemInLocalStorage('style1950')
-  if (style1950) {
-    removeItemInLocalStorage('style1950');
+  const style4 = getItemInSessionStorage(theme.STYLE_4)
+  if (style4) {
+    removeItemInSessionStorage(theme.STYLE_4);
   }
-  const style1990 = getItemInLocalStorage('style1990')
-  if (style1990) {
-    removeItemInLocalStorage('style1990');
+  const style5 = getItemInSessionStorage(theme.STYLE_5)
+  if (style5) {
+    removeItemInSessionStorage(theme.STYLE_5);
   }
-  const style2040 = getItemInLocalStorage('style2040')
-  if (style2040) {
-    removeItemInLocalStorage('style2040');
+  const style6 = getItemInSessionStorage(theme.STYLE_6)
+  if (style6) {
+    removeItemInSessionStorage(theme.STYLE_6);
   }
 
-  setItemInLocalStorage('style1920','style1920');
+  setItemInSessionStorage(theme.STYLE_3,theme.STYLE_3);
 
-  document.body.classList.toggle('style1920');
+  document.body.classList.toggle(theme.STYLE_3);
   document.querySelector('#hero-image').src="img/hero/shakespeare_1920.png";
-  document.body.classList.remove('style1600','style1890','style1950','style1990','style2040','fab-open');
+  document.body.classList.remove(
+    theme.STYLE_1,
+    theme.STYLE_2,
+    theme.STYLE_4,
+    theme.STYLE_5,
+    theme.STYLE_6,
+    'fab-open'
+  );
 
 });
 
@@ -172,32 +210,39 @@ style1920Trigger.addEventListener("click", function() {
 let style1950Trigger = document.querySelector('.trigger1950');
 style1950Trigger.addEventListener("click", function() {
 
-  const style1600 = getItemInLocalStorage('style1600')
-  if (style1600) {
-    removeItemInLocalStorage('style1600');
+  const style1 = getItemInSessionStorage(theme.STYLE_1)
+  if (style1) {
+    removeItemInSessionStorage(theme.STYLE_1);
   }
-  const style1890 = getItemInLocalStorage('style1890')
-  if (style1890) {
-    removeItemInLocalStorage('style1890');
+  const style2 = getItemInSessionStorage(theme.STYLE_2)
+  if (style2) {
+    removeItemInSessionStorage(theme.STYLE_2);
   }
-  const style1920 = getItemInLocalStorage('style1920')
-  if (style1920) {
-    removeItemInLocalStorage('style1920');
+  const style3 = getItemInSessionStorage(theme.STYLE_3)
+  if (style3) {
+    removeItemInSessionStorage(theme.STYLE_3);
   }
-  const style1990 = getItemInLocalStorage('style1990')
-  if (style1990) {
-    removeItemInLocalStorage('style1990');
+  const style5 = getItemInSessionStorage(theme.STYLE_5)
+  if (style5) {
+    removeItemInSessionStorage(theme.STYLE_5);
   }
-  const style2040 = getItemInLocalStorage('style2040')
-  if (style2040) {
-    removeItemInLocalStorage('style2040');
+  const style6 = getItemInSessionStorage(theme.STYLE_6)
+  if (style6) {
+    removeItemInSessionStorage(theme.STYLE_6);
   }
 
-  setItemInLocalStorage('style1950','style1950');
+  setItemInSessionStorage(theme.STYLE_4,theme.STYLE_4);
 
-  document.body.classList.toggle('style1950');
+  document.body.classList.toggle(theme.STYLE_4);
   document.querySelector('#hero-image').src="img/hero/shakespeare_1950.png";
-  document.body.classList.remove('style1600','style1890','style1920','style1990','style2040','fab-open');
+  document.body.classList.remove(
+    theme.STYLE_1,
+    theme.STYLE_2,
+    theme.STYLE_3,
+    theme.STYLE_5,
+    theme.STYLE_6,
+    'fab-open'
+  );
 
 });
 
@@ -207,32 +252,39 @@ style1950Trigger.addEventListener("click", function() {
 let style1990Trigger = document.querySelector('.trigger1990');
 style1990Trigger.addEventListener("click", function() {
 
-  const style1600 = getItemInLocalStorage('style1600')
-  if (style1600) {
-    removeItemInLocalStorage('style1600');
+  const style1 = getItemInSessionStorage(theme.STYLE_1)
+  if (style1) {
+    removeItemInSessionStorage(theme.STYLE_1);
   }
-  const style1890 = getItemInLocalStorage('style1890')
-  if (style1890) {
-    removeItemInLocalStorage('style1890');
+  const style2 = getItemInSessionStorage(theme.STYLE_2)
+  if (style2) {
+    removeItemInSessionStorage(theme.STYLE_2);
   }
-  const style1920 = getItemInLocalStorage('style1920')
-  if (style1920) {
-    removeItemInLocalStorage('style1920');
+  const style3 = getItemInSessionStorage(theme.STYLE_3)
+  if (style3) {
+    removeItemInSessionStorage(theme.STYLE_3);
   }
-  const style1950 = getItemInLocalStorage('style1950')
-  if (style1950) {
-    removeItemInLocalStorage('style1950');
+  const style4 = getItemInSessionStorage(theme.STYLE_4)
+  if (style4) {
+    removeItemInSessionStorage(theme.STYLE_4);
   }
-  const style2040 = getItemInLocalStorage('style2040')
-  if (style2040) {
-    removeItemInLocalStorage('style2040');
+  const style6 = getItemInSessionStorage(theme.STYLE_6)
+  if (style6) {
+    removeItemInSessionStorage(theme.STYLE_6);
   }
 
-  setItemInLocalStorage('style1990','style1990');
+  setItemInSessionStorage(theme.STYLE_5,theme.STYLE_5);
 
-  document.body.classList.toggle('style1990');
+  document.body.classList.toggle(theme.STYLE_5);
   document.querySelector('#hero-image').src="img/hero/shakespeare_1990.png";
-  document.body.classList.remove('style1600','style1890','style1920','style1950','style2040','fab-open');
+  document.body.classList.remove(
+    theme.STYLE_1,
+    theme.STYLE_2,
+    theme.STYLE_3,
+    theme.STYLE_4,
+    theme.STYLE_6,
+    'fab-open'
+  );
 
 });
 
@@ -242,34 +294,44 @@ style1990Trigger.addEventListener("click", function() {
 let style2040Trigger = document.querySelector('.trigger2040');
 style2040Trigger.addEventListener("click", function() {
 
-  const style1600 = getItemInLocalStorage('style1600')
-  if (style1600) {
-    removeItemInLocalStorage('style1600');
+  const style1 = getItemInSessionStorage(theme.STYLE_1)
+  if (style1) {
+    removeItemInSessionStorage(theme.STYLE_1);
   }
-  const style1890 = getItemInLocalStorage('style1890')
-  if (style1890) {
-    removeItemInLocalStorage('style1890');
+  const style2 = getItemInSessionStorage(theme.STYLE_2)
+  if (style2) {
+    removeItemInSessionStorage(theme.STYLE_2);
   }
-  const style1920 = getItemInLocalStorage('style1920')
-  if (style1920) {
-    removeItemInLocalStorage('style1920');
+  const style3 = getItemInSessionStorage(theme.STYLE_3)
+  if (style3) {
+    removeItemInSessionStorage(theme.STYLE_3);
   }
-  const style1950 = getItemInLocalStorage('style1950')
-  if (style1950) {
-    removeItemInLocalStorage('style1950');
+  const style4 = getItemInSessionStorage(theme.STYLE_4)
+  if (style4) {
+    removeItemInSessionStorage(theme.STYLE_4);
   }
-  const style1990 = getItemInLocalStorage('style1990')
-  if (style1990) {
-    removeItemInLocalStorage('style1990');
+  const style5 = getItemInSessionStorage(theme.STYLE_5)
+  if (style5) {
+    removeItemInSessionStorage(theme.STYLE_5);
   }
 
-  setItemInLocalStorage('style2040','style2040');
+  setItemInSessionStorage(theme.STYLE_6,theme.STYLE_6);
 
-  document.body.classList.toggle('style2040');
+  document.body.classList.toggle(theme.STYLE_6);
   document.querySelector('#hero-image').src="img/hero/shakespeare_2040.png";
-  document.body.classList.remove('style1600','style1890','style1920','style1950','style1990','fab-open');  
+  document.body.classList.remove(
+    theme.STYLE_1,
+    theme.STYLE_2,
+    theme.STYLE_3,
+    theme.STYLE_4,
+    theme.STYLE_5,
+    'fab-open'
+  ); 
 
 });
+
+
+
 
 function docReady(fn) {
     // see if DOM is already available
@@ -281,56 +343,74 @@ function docReady(fn) {
     }
 }
 
-function log(){console.log("ready")};
+function applyTheme() {
+// I retrieve the items from the session storage and go to test them, if it matches I apply it
+  const style1 = getItemInSessionStorage(theme.STYLE_1)
+  const style2 = getItemInSessionStorage(theme.STYLE_2)
+  const style3 = getItemInSessionStorage(theme.STYLE_3)
+  const style4 = getItemInSessionStorage(theme.STYLE_4)
+  const style5 = getItemInSessionStorage(theme.STYLE_5)
+  const style6 = getItemInSessionStorage(theme.STYLE_6)
+  
 
-function setItemInLocalStorage(key,value) {
-  const itemCheck = localStorage.getItem(key)  
+  switch (true) {
+    case (theme.STYLE_1 === style1):
+      document.body.classList.toggle(theme.STYLE_1);
+      document.querySelector('#hero-image').src="img/hero/shakespeare_1600.png";
+    break;
+
+    case (theme.STYLE_2 === style2):
+      document.body.classList.toggle(theme.STYLE_2);
+      document.querySelector('#hero-image').src="img/hero/shakespeare_1890.png";
+    break;
+
+    case (theme.STYLE_3 === style3):
+      document.body.classList.toggle(theme.STYLE_3);
+      document.querySelector('#hero-image').src="img/hero/shakespeare_1920.png";
+    break;
+
+    case (theme.STYLE_4 === style4):
+      document.body.classList.toggle(theme.STYLE_4);
+      document.querySelector('#hero-image').src="img/hero/shakespeare_1950.png";
+    break;
+
+    case (theme.STYLE_5 === style5):
+      document.body.classList.toggle(theme.STYLE_5);
+      document.querySelector('#hero-image').src="img/hero/shakespeare_1990.png";
+    break;
+
+    case (theme.STYLE_6 === style6):
+      document.body.classList.toggle(theme.STYLE_6);
+      document.querySelector('#hero-image').src="img/hero/shakespeare_2040.png";
+    break;
+
+    default:
+      document.body.classList.toggle(theme.STYLE_1);
+      document.querySelector('#hero-image').src="img/hero/shakespeare_1600.png";
+      setItemInSessionStorage(theme.STYLE_1,theme.STYLE_1);
+  }
+
+} 
+
+function setItemInSessionStorage(key,value) {
+  const itemCheck = sessionStorage.getItem(key)  
 
   if(!itemCheck){
-    localStorage.setItem(key, value)
+    sessionStorage.setItem(key, value)
   } else {
   return
   }
 }
 
-function applyTheme() {
-  const style1600 = getItemInLocalStorage('style1600')
-  const style1890 = getItemInLocalStorage('style1890')
-  const style1920 = getItemInLocalStorage('style1920')
-  const style1950 = getItemInLocalStorage('style1950')
-  const style1990 = getItemInLocalStorage('style1990')
-  const style2040 = getItemInLocalStorage('style2040')
-
-    if (style1600) {
-      document.body.classList.toggle(style1600);
-      document.querySelector('#hero-image').src="img/hero/shakespeare_1600.png";
-    } else if (style1890) {
-      document.body.classList.toggle(style1890);
-      document.querySelector('#hero-image').src="img/hero/shakespeare_1890.png";
-    } else if (style1920) {
-      document.body.classList.toggle(style1920);
-      document.querySelector('#hero-image').src="img/hero/shakespeare_1920.png";
-    } else if (style1950) {
-      document.body.classList.toggle(style1950);
-      document.querySelector('#hero-image').src="img/hero/shakespeare_1950.png";
-    } else if (style1990) {
-      document.body.classList.toggle(style1990);
-      document.querySelector('#hero-image').src="img/hero/shakespeare_1990.png";
-    } else if (style2040) {
-      document.body.classList.toggle(style2040);
-      document.querySelector('#hero-image').src="img/hero/shakespeare_2040.png";
-    }
-} 
-
-function getItemInLocalStorage(key) {
-  return localStorage.getItem(key)  
+function getItemInSessionStorage(key) {
+  return sessionStorage.getItem(key)  
 }
 
-function removeItemInLocalStorage(key) {
-  const itemTrovato = localStorage.getItem(key)
+function removeItemInSessionStorage(key) {
+  const itemTrovato = sessionStorage.getItem(key)
 
   if(itemTrovato){
-    localStorage.removeItem(key)
+    sessionStorage.removeItem(key)
   } else {
   return
   } 
@@ -354,8 +434,11 @@ function sidebarDetect() {
 
 sidebarDetect()
 
+
+
 $( document ).ready(function() {
 
+  console.log( "ready!" );
 
   $( ".header__icon" ).on('click', function() {
     $('.header__menu').toggleClass('header__menu--open');
@@ -371,10 +454,6 @@ $( document ).ready(function() {
   $(window).load(function(){
     $('.preloader').fadeOut('fast');
     $('.header, .aside').addClass('is-inview');
-    // scroll.start();
-  // $(".fab-trigger").click(function(){
-  //   $(".overlay").fadeIn(1000);
-  // });
 
   });
 
