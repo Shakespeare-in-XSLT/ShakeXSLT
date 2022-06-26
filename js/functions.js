dce3bcd72cb2576da914ba51ea435a25e447c804
olgagolgan <67375574+olgagolgan@users.noreply.github.com>
2022-06-26 17:54:22 +0200

    comments adjustments for Olga's code

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,6 @@
+// create dictionary with userclasses
 var UserClasses_init = {}
+// transform it in JSON object and save it into localStorage
 localStorage.setItem('UserClasses', JSON.stringify(UserClasses_init))
 
 $(document).ready(main);
@@ -39,103 +41,82 @@ function main() {
     
     }
     )
-
+    
+    // reads params from query in url and calls function to upload text   
     parametri = getParams(document.URL)
-    if (parametri['ConttoLoad'] == 'Gentlemen') {
-        displayResultGentlemen()
-
+    if (parametri['ConttoLoad'] == 'Errors') {
+        displayResult("./xml/TheComedyOfErrors.xml", "./xml/template.xsl");
     }
     else if (parametri['ConttoLoad'] == 'Tempest'){
-        displayResultTempest()
-       
+        displayResult("./xml/TheTempest.xml", "./xml/template.xsl");
     }
-    else if (parametri['ConttoLoad'] == 'Errors'){
-        displayResultErrors()
-        
+    else if (parametri['ConttoLoad'] == 'Gentlemen'){
+        displayResult("./xml/TwoGentlemenOfVerona.xml", "./xml/template.xsl");   
     }
 };
 
-
+// empty div content
 function reset() {
     $("#example").empty();
 }
 
 function loadXMLDoc(filename) {
+    // code for IE, it doesn't enter in this function in newer browsers, but just in case...
     if (window.ActiveXObject) {
-        xhttp = new ActiveXObject("Msxml2.XMLHTTP");
+        xhttp = new ActiveXObject("Msxml2.XMLHTTP"); 
     }
+    // create new XMLHttpRequest object with GET method
     else {
         xhttp = new XMLHttpRequest();
     }
     xhttp.open("GET", filename, false);
     try { xhttp.responseType = "msxml-document" } catch (err) { } // Helping IE11
+    // send an empty request
     xhttp.send("");
+    // return an xhttp response
     return xhttp.responseXML;
 }
 
-
-
-function displayResultErrors() {
-    xml = loadXMLDoc("./xml/TheComedyOfErrors.xml");
-    xsl = loadXMLDoc("./xml/template.xsl");
+/**
+* Gets specific XML and XSL names, process and append on current document page
+* @param {String} xmlName
+* @param {String} xslName
+*/
+function displayResult(xmlName, xslName) {
+    // call the function for initiating an http request
+    xml = loadXMLDoc(xmlName);
+    xsl = loadXMLDoc(xslName);
     // code for IE
     if (window.ActiveXObject || xhttp.responseType == "msxml-document") {
         reset()
-        ex = xml.transformNode(xsl);
+        ex = xml.transformNode(xsl); 
+        // Processes this node and its children using the supplied xslt and returns the resulting transformation
         document.getElementById("example").innerHTML = ex;
+        // calling function to create browsable index and get title of text
         new_index()
         getTitle()
     }
     // code for Chrome, Firefox, Opera, etc.
     else if (document.implementation && document.implementation.createDocument) {
+        // empty the destination div
         reset()
+        // instantiating new XSLTprocessor object
         xsltProcessor = new XSLTProcessor();
+        // import xslt through method
         xsltProcessor.importStylesheet(xsl);
+        // transforming xml through xslt into destination document
         resultDocument = xsltProcessor.transformToFragment(xml, document);
         document.getElementById("example").appendChild(resultDocument);
+        // calling function to create browsable index and get title of text
         new_index()
         getTitle()
     }
+    // check if user from mobile device
     var isMobile = false;
     if (/Mobi|Android/i.test(navigator.userAgent)) {
         isMobile = true; // mobile!
         alert('Hey you! Flip your device to surf the metadata!')}
-
-    else {
-        if(window.matchMedia("(max-width: 600px)").matches){
-        alert ('Hey you! Maximize a little your browser window to surf the metadata!')
-        }
-    }
-
-}
-
-
-function displayResultTempest() {
-    xml = loadXMLDoc("./xml/TheTempest.xml");
-    xsl = loadXMLDoc("./xml/template.xsl");
-    // code for IE
-    if (window.ActiveXObject || xhttp.responseType == "msxml-document") {
-        reset()
-        ex = xml.transformNode(xsl);
-        document.getElementById("example").innerHTML = ex;
-        new_index()
-        getTitle()
-    }
-    // code for Chrome, Firefox, Opera, etc.
-    else if (document.implementation && document.implementation.createDocument) {
-        reset()
-        xsltProcessor = new XSLTProcessor();
-        xsltProcessor.importStylesheet(xsl);
-        resultDocument = xsltProcessor.transformToFragment(xml, document);
-        document.getElementById("example").appendChild(resultDocument);
-        new_index()
-        getTitle()
-    }
-     var isMobile = false;
-    if (/Mobi|Android/i.test(navigator.userAgent)) {
-        isMobile = true; // mobile!
-        alert('Hey you! Flip your device to surf the metadata!')}
-
+    // check if user from desktop with smaller window
     else {
         if(window.matchMedia("(max-width: 600px)").matches){
         alert ('Hey you! Maximize a little your browser window to surf the metadata!')
@@ -143,39 +124,6 @@ function displayResultTempest() {
     }
 }
 
-
-function displayResultGentlemen() {
-    xml = loadXMLDoc("./xml/TwoGentlemenOfVerona.xml");
-    xsl = loadXMLDoc("./xml/template.xsl");
-    // code for IE
-    if (window.ActiveXObject || xhttp.responseType == "msxml-document") {
-        reset()
-        ex = xml.transformNode(xsl);
-        document.getElementById("example").innerHTML = ex;
-        new_index()
-        getTitle()
-    }
-    // code for Chrome, Firefox, Opera, etc.
-    else if (document.implementation && document.implementation.createDocument) {
-        reset()
-        xsltProcessor = new XSLTProcessor();
-        xsltProcessor.importStylesheet(xsl);
-        resultDocument = xsltProcessor.transformToFragment(xml, document);
-        document.getElementById("example").appendChild(resultDocument);
-        new_index()
-        getTitle()
-    }
-     var isMobile = false;
-    if (/Mobi|Android/i.test(navigator.userAgent)) {
-        isMobile = true; // mobile!
-        alert('Hey you! Flip your device to surf the metadata!')}
-
-    else {
-        if(window.matchMedia("(max-width: 600px)").matches){
-        alert ('Hey you! Maximize a little your browser window to surf the metadata!')
-        }
-    }
-}
 
 /**
 * Gets params from url as an object
@@ -185,24 +133,26 @@ function displayResultGentlemen() {
 var getParams = function (url) {
     var params = {};
     var match = url.match(/\?(.*)$/);
-
     if (match && match[1]) {
         match[1].split('&').forEach(function (pair) {
             pair = pair.split('=');
             params[pair[0]] = pair[1];
         });
     }
-
     return params;
 };
 
 function new_index(){
+    // save the title with specific class
     var bau = $("h2[class='act']")
+    //create paragraph with id of specific act and associate function on click to scroll to that specific part of text
     for (var i=0; i<bau.length; i++){
         $("#att").append("<p id='" + String(i+1) + "' onclick=jumpto('act_" + String(i+1) + "')>" + bau[i].innerText + "</p>")
+        // create an ul corresponding to the scenes of that act
         $("#att").append("<ul id='scene_" + String(i+1) + "'></ul>")
         var miao = $("[id*='a"+String(i+1)+"_s']");
         for (var y=0; y<miao.length; y++){
+            // create an element of the list for every scene assciated on click to function 
             $("#scene_" + String(i+1)).append("<li onclick=jumpto('a"+ String(i+1) + "_s" + String(y+1) + "')>"+ miao[y].innerText + "</li>")   
         }
     }
@@ -215,39 +165,47 @@ function getTitle(){
     $("#title").css({"display": "none"});
     }
 }
-        
+
+// scroll to specific info        
 function jumpto(thediv){
     document.getElementById(thediv).scrollIntoView({behavior: 'smooth'});
 }
 
 function saveNewClass(){
+    // instantiate a new variable corresponding to the input of the user
     NewClass= $("#insert").val().toLowerCase().toString();
+    // save the selection of the document and transform it to string
     sel = document.getSelection()
     Value = sel.toString();
+    // preserving whitespacing
     var ciao = Value.replaceAll("\n", "</br>");
+    // if NewClass or Value is empty string stop execution
     if ((NewClass == '') | (NewClass == null) | (Value == '') | (Value == null)) {
-        // if one is empty string stop execution
         alert("Select a valid text to create the new class!")
     }
     else{  // download exising User classes
         UserClasses = JSON.parse(localStorage.getItem('UserClasses'))
-        // if new class --> create empty list of values
+        // if NewClass is not in Userclasses --> create empty list of values
         if ((NewClass in UserClasses)==0){
             UserClasses[NewClass] = []
         } 
-        // append new value
+        // append new value to UserClasses
         UserClasses[NewClass].push(Value);
-        // upload updated User classes
+        // save updated UserClasses
         localStorage.setItem('UserClasses', JSON.stringify(UserClasses));
         // update formcheck list
         updateformcheckList(Object.keys(UserClasses));
-        // insert tag for selection
+        // create tag for selected text
         var spn = document.createElement('span')
         spn.innerHTML = ciao;
+        //add class NewClass and "myspan" to element
         spn.classList.add(NewClass);
         spn.classList.add("myspan");
+        // var range containing the starting range of the selection
         range = sel.getRangeAt(0);
+        // clear the document content at that range
         range.deleteContents();
+        // insert the new span node
         range.insertNode(spn);
 
         //to handle multiple selections for the same class
@@ -263,14 +221,18 @@ function saveNewClass(){
 }
 
 function updateformcheckList(lista){
+    // save a list of checkbox present in the html and instantiate an empty list
     elementipresentiJQ = $("#formchecks").children(".form-check")
     elementipresenti = []
+    // append the value of the label of every checkbox to empty list
     for (var i=0; i<elementipresentiJQ.length; i++){
         elementipresenti.push($(elementipresentiJQ[i].firstChild).val())
     }
+    // for every element not present in the list, create a new checkbox
     for (let ele of lista){
         if (elementipresenti.indexOf(ele)==-1){
             $("#formchecks").append("<div class='form-check'><input class='form-check-input' type='checkbox' value='" + ele + "' id='"+ele+"' /><label class='form-check-label'>" + ele + "</label></div>")
+            // on click --> function which checks if checbox is checked or not (adding a class and styling backgroud if yes, removing class otherwise)
             $("#formchecks").find(".form-check-input[value='" + ele + "']").click(function () {
                 if (this.checked) {
                     $('.'+ele).addClass('userclassSelected')
@@ -284,11 +246,9 @@ function updateformcheckList(lista){
     }}
 
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+//When the user clicks on the button, toggle between hiding and showing the dropdown content
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
-  
 }
 
 // Close the dropdown menu if the user clicks outside of it
